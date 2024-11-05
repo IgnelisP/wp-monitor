a0396c59d73388238842df1d79a1e248793ca155
IgnelisP <ignas.povi@gmail.com>
2024-11-05 18:02:57 +0000

    plugins, tables, working as expected. Communication via API keys

--- app/Http/Controllers/PluginsController.php
+++ app/Http/Controllers/PluginsController.php
@@ -10,23 +10,33 @@ class PluginsController extends Controller
 {
     public function enable(Website $website, $slug)
     {
-        $response = Http::post("{$website->full_url}/wp-json/wp-monitor-bridge/v1/plugin/enable/{$slug}");
+        $apiKey = $website->api_key;
+        $response = Http::withHeaders([
+            'X-WPMB-API-Key' => $apiKey,
+        ])->post("{$website->full_url}/wp-json/wp-monitor-bridge/v1/plugin/enable/{$slug}");
 
         if ($response->successful()) {
-            return response()->json(['message' => 'Plugin enabled successfully.']);
+            $message = $response->json('status', 'Plugin enabled successfully.');
+            return response()->json(['message' => $message]);
         }
 
-        return response()->json(['message' => 'Failed to enable plugin.'], 500);
+        $error = $response->json('error', 'Failed to enable plugin.');
+        return response()->json(['message' => $error], 500);
     }
 
     public function disable(Website $website, $slug)
     {
-        $response = Http::post("{$website->full_url}/wp-json/wp-monitor-bridge/v1/plugin/disable/{$slug}");
+        $apiKey = $website->api_key;
+        $response = Http::withHeaders([
+            'X-WPMB-API-Key' => $apiKey,
+        ])->post("{$website->full_url}/wp-json/wp-monitor-bridge/v1/plugin/disable/{$slug}");
 
         if ($response->successful()) {
-            return response()->json(['message' => 'Plugin disabled successfully.']);
+            $message = $response->json('status', 'Plugin disabled successfully.');
+            return response()->json(['message' => $message]);
         }
 
-        return response()->json(['message' => 'Failed to disable plugin.'], 500);
+        $error = $response->json('error', 'Failed to disable plugin.');
+        return response()->json(['message' => $error], 500);
     }
 }
